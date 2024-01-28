701353a22b28761c3e7430dfc135d4d3702b6a90
randyChrisp <79rchrisp@gmail.com>
2024-01-28 01:01:24 -0500

    push untracked

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -1,7 +1,12 @@
-import {AutoComplete} from "../components/AutoComplete"
-import {StockList} from "../components/StockList"
+import { AutoComplete } from "../components/AutoComplete"
+import { StockList } from "../components/StockList"
+import graph from "../images/graph.png"
+
 export const StockOverviewPage = () => {
-  return <div>StockOverviewPage
+  return <div>
+    <div>
+      <img className="graph" src={graph} />
+    </div>
     <AutoComplete />
     <StockList />
   </div>
